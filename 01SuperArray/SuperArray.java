7d3268052b814485b18513ac387044d6c29d9878
yankees2 <brianwontonsoup@gmail.com>
2014-11-12 00:50:05 -0500

    Update SuperArray.java

--- 01SuperArray/SuperArray.java
+++ 01SuperArray/SuperArray.java
@@ -10,10 +10,14 @@ public class SuperArray{
 	array = new Object[x];
     }
 
+    public int getCap(){
+	return array.length;
+    }
+
     public String toString(){
 	String contents = "[ ";
 	int index = 0;
-	while (index < array.length) {
+	while (index < size()){
 	    contents = contents + array[index] + " ";
 	    index++;
 	}
@@ -22,41 +26,42 @@ public class SuperArray{
     }
 
     public void add(int index, Object o){
-	Object[] holder;
-	if (index >= array.length){
-	    holder = new Object[index+1];
-	    holder[index] = o;
-	    int a = 0;
-	    while (a < array.length){
-		holder[a] = array[a];
-		a++;
-	    }
-	} else {
-	    int newLength = array.length + 1;
-	    holder = new Object[newLength];
-	    holder[index] = o;
-	    int a = 0;
-	    while (a < index){
-		holder[a] = array[a];
-		a++;
-	    }
-	    int b = index + 1;
-	    while (b < holder.length){
-		holder[b] = array[b-1];
-		b++;
-	    }
+	if (size()==array.length-1){
+	    resize(2*array.length);
 	}
-	array = holder;
+	if (index >= 0 && index < size()){
+	    Object[] holder = new Object[array.length];
+	    int x = 0;
+	    while (x<index){
+		holder[x] = array[x];
+		x++;
+	    }
+	    holder[index] = o;
+	    int y = index;
+	    while (y<size()){
+		holder[y+1] = array[y];
+		y++;
+	    }
+	    array = holder;
+	} else if (index==size()){
+	    array[index] = o;
+	} else {
+	    throw new IndexOutOfBoundsException();
+	}
     }
 
     public int size(){
-	return array.length;
+	int x = 0;
+	while (array[x]!=null){
+	    x++;
+	}
+	return x;
     }
 
     public void resize(int newCapacity){
 	Object[] holder = new Object[newCapacity];
 	int a = 0;
-	while (a < newCapacity) {
+	while (a < size()) {
 	    holder[a] = array[a];
 	    a++;
 	}
@@ -70,17 +75,15 @@ public class SuperArray{
 
     public Object get(int index){
 	if (index < 0 || index >= size()){
-	    System.out.println("Index is out of range");
-	    return null;
+	    throw new IndexOutOfBoundsException();
 	} else {
 	    return array[index];
 	}
     }
 
     public Object set(int index, Object o){
-	if (index < 0 || index >= array.length){
-	    System.out.println("Index is out of range");
-	    return null;
+	if (index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
 	} else {
 	    Object replaced = array[index];
 	    array[index] = o;
@@ -89,12 +92,14 @@ public class SuperArray{
     }
 
     public Object remove(int index){
-	if (index < 0 || index >= array.length){
-	    System.out.println("Index is out of range");
-	    return null;
+	if (index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
 	} else {
+	    if (size()==(array.length/4 + 1)){
+		resize(array.length/2);
+	    }
 	    Object removed = array[index];
-	    Object[] holder = new Object[array.length-1];
+	    Object[] holder = new Object[array.length];
 	    int a = 0;
 	    while (a < index){
 		holder[a] = array[a];
